8c67151676c0d59f719f1d8d43c7559d5efa5561
Christoph M. Becker <cmbecker69@gmx.de>
2020-10-04 23:58:12 +0200

    Use proper configure option

--- .appveyor/build.ps1
+++ .appveyor/build.ps1
@@ -4,7 +4,7 @@ Set-Location 'C:\projects\xmlrpc'
 
 $task = New-Item 'task.bat' -Force
 Add-Content $task "call phpize 2>&1"
-Add-Content $task "call configure --enable-xmlrpc 2>&1"
+Add-Content $task "call configure --with-xmlrpc 2>&1"
 Add-Content $task "nmake /nologo 2>&1"
 Add-Content $task "exit %errorlevel%"
 & "C:\build-cache\php-sdk-$env:BIN_SDK_VER\phpsdk-$env:VC-$env:ARCH.bat" -t $task
